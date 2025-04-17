b9cd8cafa6fd99ebe7175f3bf47231afa5585a5d
Marcos Amorim <nutriamorim69@gmail.com>
2025-04-17 15:14:57 -0300

    Remove urls.py duplicado e atualiza rotas dos apps

--- loja_app/urls.py
+++ loja_app/urls.py
@@ -2,5 +2,39 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('', views.home, name='home')
-]
+    path('', views.home, name='home'),
+    path('nutricao_para_atletas/', views.nutricao_para_atletas, name='nutricao_para_atletas'),
+    path('suplementacao_esportiva/', views.suplementacao_esportiva, name='suplementacao_esportiva'),
+    path('protocolo_de_treino/', views.protocolo_de_treino, name='protocolo_de_treino'),
+    path('recuperacao_muscular/', views.recuperacao_muscular, name='recuperacao_muscular'),
+
+    # Nutrição Funcional
+    path('alimentacao_alcalina/', views.alimentacao_alcalina, name='alimentacao_alcalina'),
+    path('fitoterapia/', views.fitoterapia, name='fitoterapia'),
+    path('protocolos_anti_inflamatorios/', views.protocolos_anti_inflamatorios, name='protocolos_anti_inflamatorios'),
+    path('alimentos_funcionais/', views.alimentos_funcionais, name='alimentos_funcionais'),
+
+    # Objetivos
+    path('massa_magra/', views.massa_magra, name='massa_magra'),
+    path('definicao_muscular/', views.definicao_muscular, name='definicao_muscular'),
+    path('emagrecimento/', views.emagrecimento, name='emagrecimento'),
+    path('energia_disposicao/', views.energia_disposicao, name='energia_disposicao'),
+
+    # Receitas Medicinais
+    path('receitas1/', views.receitas1, name='receitas1'),
+    path('receitas2/', views.receitas2, name='receitas2'),
+
+    # Pesquisas Científicas
+    path('pesquisas1/', views.pesquisas1, name='pesquisas1'),
+    path('pesquisas2/', views.pesquisas2, name='pesquisas2'),
+
+    # Consultas
+    path('agendamento_online/', views.agendamento_online, name='agendamento_online'),
+    path('avaliacao_fisica/', views.avaliacao_fisica, name='avaliacao_fisica'),
+    path('planos_alimentares/', views.planos_alimentares, name='planos_alimentares'),
+
+    # Institucional
+    path('quem_sou_eu/', views.quem_sou_eu, name='quem_sou_eu'),
+    path('quem_somos_nos/', views.quem_somos_nos, name='quem_somos_nos'),
+    ]
+
--- loja_nutri/urls.py
+++ loja_nutri/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls')),  # Ou use 'auth/' se preferir
-    path('', include('loja_app.urls')),           # Página inicial agora vai funcionar com o home.html do loja_app
-]
+    path('', include('loja_app.urls')),       # URLs do app loja_app (onde está sua página principal)
+    path('usuarios/', include('usuarios.urls')),  # URLs do app de autenticação
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
