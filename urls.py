ebb90a8405bc12c8f668866ffc2fb8ea79e15803
Marcos Amorim <nutriamorim69@gmail.com>
2025-04-17 16:41:26 -0300

    Alteração no arquivo urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls')),  # URLs do app 'usuarios'
-    path('', include('loja_nutri.urls')),  # URLs do app 'loja_nutri' (a página inicial do site)
-]
+    path('', include('loja_nutri.urls')),  # URLs do app 'loja_nutri' (página inicial)
+    path('usuarios/', include('usuarios.urls')),  # URLs do app de autenticação
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
