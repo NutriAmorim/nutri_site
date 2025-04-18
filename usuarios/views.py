d060f04ffa25cbfa797caf381a4212706b61a09e
Marcos Amorim <nutriamorim69@gmail.com>
2025-04-17 23:38:30 -0300

    Correções e melhorias no sistema de login, cadastro e logout

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
 
 # Função para login
 def login_view(request):
